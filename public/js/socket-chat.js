2d68cf13f2c0d1589aae25c84af659a908a1a797
danbart <danilo_solorzano06@hotmail.com>
2019-03-21 22:41:28 -0600

    cargar estilo

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,21 +1,24 @@
 var socket = io();
 
 var params = new URLSearchParams(window.location.search);
+
 if (!params.has('nombre') || !params.has('sala')) {
     window.location = 'index.html';
-    throw new Error('El Nombre y sala son necesarios');
+    throw new Error('El nombre y sala son necesarios');
 }
 
 var usuario = {
     nombre: params.get('nombre'),
     sala: params.get('sala')
-}
+};
+
+
 
 socket.on('connect', function() {
     console.log('Conectado al servidor');
 
-    socket.emit('entrarChat', usuario, function(res) {
-        console.log('Usuarios conectados ', res);
+    socket.emit('entrarChat', usuario, function(resp) {
+        console.log('Usuarios conectados', resp);
     });
 
 });
@@ -29,30 +32,27 @@ socket.on('disconnect', function() {
 
 
 // Enviar información
-/* socket.emit('enviarMensaje', {
-    usuario: 'Fernando',
-    mensaje: 'Hola Mundo'
-}, function(resp) {
-    console.log('respuesta server: ', resp);
-}); */
+// socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
     console.log('Servidor:', mensaje);
-
 });
 
-// escuchar cambios de usuarios
-//cuando un usuario entra o sale del chat
-
-socket.on('listaPersonas', function(mensaje) {
-
-    console.log(mensaje);
-
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
+socket.on('listaPersona', function(personas) {
+    console.log(personas);
 });
 
 // Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
-    console.log('Mensaje  Privado', mensaje);
+
+    console.log('Mensaje Privado:', mensaje);
+
 });
